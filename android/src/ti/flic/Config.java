7459c00dddb2f0a035f6d08adfd31295d9c3dfc7
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-11-29 21:25:50 +0100

    refactoring in configure class

--- android/src/ti/flic/Config.java
+++ android/src/ti/flic/Config.java
@@ -9,9 +9,8 @@ public class Config {
 	static void setFlicCredentials() {
 		TiProperties appProperties = TiApplication.getInstance()
 				.getAppProperties();
-		String appId = appProperties.getString("FLIC_ID", "");
-		String appSecret = appProperties.getString("FLIC_SECRET", "");
-		String appName = appProperties.getString("FLIC_NAME", "");
-		FlicManager.setAppCredentials(appId, appSecret, appName);
+		FlicManager.setAppCredentials(appProperties.getString("FLIC_ID", ""),
+				appProperties.getString("FLIC_SECRET", ""),
+				appProperties.getString("FLIC_NAME", ""));
 	}
 }
